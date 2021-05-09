feacc256cc23dbb18fb33dc155563150258a0735
filipef9 <filipef9@hotmail.com>
2021-05-09 22:47:36 +0000

    Introdução ao Angular 8
    2. Lidando com vários componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,44 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    NavBarComponent,
+    ReplacePipe,
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full'
+      },
+      {
+        path: 'courses/info/:id',
+        component: CourseInfoComponent
+      },
+      {
+        path: 'courses',
+        component: CourseListComponent
+      },
+      {
+        path: '**',
+        component: Error404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
